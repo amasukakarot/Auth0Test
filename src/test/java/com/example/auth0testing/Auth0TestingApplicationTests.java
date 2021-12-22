b505020efbb83350ac28efd655f449b442041a0f
Usama Javed <usamajaved@Usamas-MacBook-Pro.local>
2021-12-22 19:15:53 +0000

    initial commit

---
+++ src/test/java/com/example/auth0testing/Auth0TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth0testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0TestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
